a83e34680340dc5fee794bfe044621bca6924538
jaikeerthi2617 <jaikeerthi.official@gmail.com>
2026-06-02 15:47:47 +0530

    Cleaned up

--- dbt/macros/generate_surrogate_key.sql
+++ dbt/macros/generate_surrogate_key.sql
@@ -8,11 +8,3 @@
         )
     )
 {% endmacro %}
-
-{% macro generate_schema_name(custom_schema_name, node) -%}
-    {%- if custom_schema_name is none -%}
-        {{ target.schema }}
-    {%- else -%}
-        {{ custom_schema_name | trim }}
-    {%- endif -%}
-{%- endmacro %}
